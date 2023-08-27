fd8bc1a8285763a2f9031e146b7300b56925f9de
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-28 01:37:11 +0530

    create react app and create navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
-      
+     <NavBar/>
     </div>
   );
 }
